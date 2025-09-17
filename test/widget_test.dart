cdc861e3bbf428dd39b26a44c22cbacd66c7bfaa
tozpeak <tozpeak@gmail.com>
2025-09-17 15:56:24 +0300

    implemented smoke test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,22 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_test_solidsoft/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('GestureDetector tap smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verify text exists
+    expect(find.text('Hello there'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    Color startColor = tester.widget<ColoredBox>(find.byType(ColoredBox)).color;
+    // Tap anywhere at GestureDetector.
+    await tester.tap(find.byType(GestureDetector));
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    //Wait for animation
+    await tester.pump(Durations.short1);
+
+    Color afterTapColor = tester.widget<ColoredBox>(find.byType(ColoredBox)).color;
+    expect(afterTapColor, isNot(startColor));
   });
 }
